f6a0294949eaf6649d78480e4b9868f1d1fb18ec
Paco Nathan <ceteri@gmail.com>
2016-10-05 08:46:33 -0700

    fixed labels in Dot file

--- textrank.py
+++ textrank.py
@@ -260,7 +260,7 @@ def write_dot (graph, ranks, path="graph.dot"):
   dot = Digraph()
 
   for node in graph.nodes():
-    dot.node(node, "%0.3f" % (ranks[node]))
+    dot.node(node, "%s %0.3f" % (node, ranks[node]))
 
   for edge in graph.edges():
     dot.edge(edge[0], edge[1], constraint="false")
