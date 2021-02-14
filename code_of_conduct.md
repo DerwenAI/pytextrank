03b4208f8be0cb06a3ad94a97324e959d2d06dda
Paco Nathan <ceteri@gmail.com>
2021-02-14 14:47:57 -0800

    prep release 3.0.0

--- code_of_conduct.md
+++ code_of_conduct.md
@@ -54,7 +54,7 @@ This Code of Conduct applies within all project spaces, and it also
 applies when an individual is representing the project or its
 community in public spaces.
 Examples of representing a project or community include using an
-official project e-mail address, posting via an official social media
+official project email address, posting via an official social media
 account, or acting as an appointed representative at an online or
 offline event.
 Representation of a project may be further defined and clarified by
---
+++ pytextrank/version.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import typing
+
+
+######################################################################
+## Python version checking
+
+MIN_PY_VERSION: typing.Tuple = (3, 7,)
+__version__: str = "3.0.0"
+
+
+def _versify (
+    py_version_info: typing.Tuple
+    ) -> str:
+    """
+Semiprivate helper function to convert Python version to a point release (a string).
+
+    py_version_info:
+Python version info as a named tuple from the operating system, e.g., from [`sys.version_info[:2]`](https://docs.python.org/3/library/sys.html#sys.version_info)
+
+    returns:
+Python version info in [*semantic versioning*](https://semver.org/) format
+    """
+    return ".".join([ str(x) for x in py_version_info ])
+
+
+def _check_version () -> None:
+    """
+Semiprivate helper function to check the Python version info versus the minimum required for **pytextrank**.
+Throws a `RuntimeError` if the installed Python interpreter is out of date.
+    """
+    py_version_info: typing.Tuple = sys.version_info[:2]
+
+    if py_version_info < MIN_PY_VERSION:
+        error_msg = "This version of pytextrank requires Python {} or later ({} detected)\n"
+        raise RuntimeError(error_msg.format(_versify(MIN_PY_VERSION), _versify(py_version_info)))
